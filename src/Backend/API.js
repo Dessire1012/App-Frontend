32e1d4784c55651336955b9534be767a52cbc5b0
Dessire1012 <dessire@unitec.edu>
2024-09-11 01:18:19 -0600

    Cambio axios a fetch

--- src/Backend/API.js
+++ src/Backend/API.js
@@ -1,12 +1,19 @@
-import axios from "axios";
-
 const API_URL =
   "https://app-ffb84f79-a617-43e4-b3ef-d4e15dbc138f.cleverapps.io/user";
 
 export const loginUser = async (credentials) => {
   try {
-    const response = await axios.post(`${API_URL}/login`, credentials);
-    return response.data;
+    const response = await fetch(`${API_URL}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(credentials),
+    });
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return await response.json();
   } catch (error) {
     console.error("Error logging in:", error);
     throw error;
@@ -15,8 +22,17 @@ export const loginUser = async (credentials) => {
 
 export const registerUser = async (userData) => {
   try {
-    const response = await axios.post(`${API_URL}/register`, userData);
-    return response.data;
+    const response = await fetch(`${API_URL}/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return await response.json();
   } catch (error) {
     console.error("Error registering:", error);
     throw error;
@@ -25,8 +41,13 @@ export const registerUser = async (userData) => {
 
 export const getUserById = async (id) => {
   try {
-    const response = await axios.get(`${API_URL}/viewUser?id=${id}`);
-    return response.data;
+    const response = await fetch(`${API_URL}/viewUser?id=${id}`, {
+      method: "GET",
+    });
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return await response.json();
   } catch (error) {
     console.error("Error fetching user data:", error);
     throw error;
