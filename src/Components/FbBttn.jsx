e01c6814224dc12b6ffc627c99d05707511044b9
Dessire1012 <dessire@unitec.edu>
2024-09-13 21:37:08 -0600

    Async

--- src/Components/FbBttn.jsx
+++ src/Components/FbBttn.jsx
@@ -52,49 +52,40 @@ function FbBttn() {
 
   const handleFbLogin = () => {
     FB.login(
-      async (response) => {
+      (response) => {
         if (response.authResponse) {
-          try {
-            FB.api(
-              "/me",
-              { fields: "id,name,email,picture" },
-              async (profile) => {
-                const user_id = profile.id;
-                const name = profile.name;
-                const email = profile.email;
-                const password = generateRandomPassword();
-                const photo = profile.picture.data.url;
-
-                let user;
-                try {
-                  user = await loginUser({ email, user_id });
-                  console.log("User logged in", user);
-                } catch (loginError) {
-                  console.error(
-                    "Login failed, attempting to register",
-                    loginError
-                  );
-                  user = await registerUser({
-                    user_id,
-                    name,
-                    email,
-                    password,
-                    photo,
-                  });
-                  console.log("User registered", user);
-                }
-                navigate("/chatbot", { state: { userId: user_id } });
-              }
-            );
-          } catch (error) {
-            console.error("Operation failed", error);
-          }
+          handleFbApi();
         } else {
           console.log("User cancelled login or did not fully authorize.");
         }
       },
       { scope: "email" }
     );
+  };
+
+  const handleFbApi = async () => {
+    try {
+      FB.api("/me", { fields: "id,name,email,picture" }, async (profile) => {
+        const user_id = profile.id;
+        const name = profile.name;
+        const email = profile.email;
+        const password = generateRandomPassword();
+        const photo = profile.picture.data.url;
+
+        let user;
+        try {
+          user = await loginUser({ email, user_id });
+          console.log("User logged in", user);
+        } catch (loginError) {
+          console.error("Login failed, attempting to register", loginError);
+          user = await registerUser({ user_id, name, email, password, photo });
+          console.log("User registered", user);
+        }
+        navigate("/chatbot", { state: { userId: user_id } });
+      });
+    } catch (error) {
+      console.error("Operation failed", error);
+    }
   };
 
   return (
